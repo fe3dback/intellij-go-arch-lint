d415ba387c5362b9107e70371b7a2e83179d6be8
fe3dback <fe3dback@protonmail.com>
2021-01-23 19:35:29 +0300

    fix build version

--- src/main/kotlin/com/github/fe3dback/intellijgoarchlint/reference/GoArchComponentNameReferenceProvider.kt
+++ src/main/kotlin/com/github/fe3dback/intellijgoarchlint/reference/GoArchComponentNameReferenceProvider.kt
@@ -5,24 +5,30 @@ import com.github.fe3dback.intellijgoarchlint.psi.GoArchPsiUtils
 import com.intellij.psi.*
 import com.intellij.util.ProcessingContext
 import org.jetbrains.yaml.psi.YAMLScalarText
-import kotlin.streams.toList
 
 class GoArchComponentNameReferenceProvider: PsiReferenceProvider() {
     override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-        if (element !is YAMLScalarText) return emptyArray()
+        if (element !is YAMLScalarText) return PsiReference.EMPTY_ARRAY
         return arrayOf(GoArchComponentNameReference(element))
     }
 }
 
-private class GoArchComponentNameReference(element: YAMLScalarText): PsiPolyVariantReferenceBase<YAMLScalarText>(element) {
-    override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
-        val mapping = GoArchPsiUtils.getTopLevelMapping(element) ?: return emptyArray()
-        val componentNode = GoArchPsiUtils.getNodeByName(mapping, GoArch.specComponents) ?: return emptyArray()
+private class GoArchComponentNameReference(element: YAMLScalarText): PsiReferenceBase<YAMLScalarText>(element) {
+    override fun resolve(): PsiElement? {
+        val mapping = GoArchPsiUtils.getTopLevelMapping(element) ?: return null
+        val componentNode = GoArchPsiUtils.getNodeByName(mapping, GoArch.specComponents) ?: return null
         val componentsStream = GoArchPsiUtils.getNodeKeyValuesStream(componentNode)
 
-        return componentsStream
-            .map { PsiElementResolveResult(it) }
-            .toList()
-            .toTypedArray()
+        val firstMatch = componentsStream
+            .filter { it.key != null }
+            .filter { it.key!!.text == element.textValue }
+            .map { PsiElementResolveResult(it.key!!) }
+            .findFirst()
+
+        if (firstMatch.isEmpty) {
+            return null
+        }
+
+        return firstMatch.get().element
     }
 }
